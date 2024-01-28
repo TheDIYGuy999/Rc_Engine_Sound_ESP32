64289ab95434b255cba89127aedea16af2879f4d
TheDIYGuy999 <msommer74@gmail.com>
2024-01-28 12:09:20 +0100

    L120H battery calibrated

--- src/3_ESC.h
+++ src/3_ESC.h
@@ -137,9 +137,9 @@ const float RECOVERY_HYSTERESIS = 0.2;   // around 0.2 V
  * When selecting resistors, always use two of the same magnitude: Like, for example, 10k/2k, 20k/4k or 100k/20k. NEVER exceed a ratio LOWER than (4:1 = 4)!
  * WARNING: If the ratio is too LOW, like 10k/5k (2:1 = 2), the battery voltage will most likely DAMAGE the controller permanently!
  * Example calculation: 2000 / (2000 + 10000) = 0.166 666 666 7; 7.4 V * 0.167 = 1.2358 V (of 3.3 V maximum on GPIO Pin). */
-uint32_t RESISTOR_TO_BATTTERY_PLUS = 10000; // Value in Ohms (Ω), for example 10000
+uint32_t RESISTOR_TO_BATTTERY_PLUS = 9400; // Value in Ohms (Ω), for example 10000
 uint32_t RESISTOR_TO_GND = 1000;            // Value in Ohms (Ω), for example 2000. Measuring exact resistor values before soldering, if possible is recommended!
-float DIODE_DROP = 0;                       // Fine adjust measured value and/or consider diode voltage drop (about 0.34V for SS34 diode)
+float DIODE_DROP = 0.31;                       // Fine adjust measured value and/or consider diode voltage drop (about 0.34V for SS34 diode)
 /* It is recommended to add a sticker to your ESP32, which includes the 3 calibration values above */
 volatile int outOfFuelVolumePercentage = 80; // Adjust the message volume in %
 // Select the out of fuel message you want:
